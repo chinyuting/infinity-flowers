f42d0a15fe51e1d5aa2003f89fe79f3e562bb2f6
“chinyuting” <s86052915@gmail.com>
2023-04-19 20:26:03 +0800

    new arrived edit

--- src/views/Front/HomeView.vue
+++ src/views/Front/HomeView.vue
@@ -10,36 +10,58 @@
         <div class="pb-2" style="text-shadow: rgb(226, 213, 196) 2px 0 10px;">scroll</div>
       </div>
     </div>
+    <!-- 限時優惠 -->
     <div class="mt-5 container-fluid">
       <div class="on-sale w-100 text-center p-5 rounded-3">
         <h2>限時優惠 <span class="text-danger fw-bolder">８折</span></h2>
         <button class="btn btn-light" @click.prevent="openModel">點擊領取優惠券</button>
       </div>
+    <!-- 標語 -->
       <div class="w-100 text-center">
         <h3 class="p-5 fst-italic fw-lighter">花藝對很多人而言，只是曇花一現的東西，只是一時的歡愉體驗</h3>
       </div>
-      <div class="row">
-        <div class="col-md-6 align-middle" style="color:rgba(231, 152, 152, 1);">
-          <h3 class="fst-italic slogan text-center m-auto">
-            乾燥花是時間的見證<br>它記錄了花的美麗瞬間<br>留下了永恆的回憶。
-            <!-- position-absolute top-50 start-50 translate-middle -->
-          </h3>
-        </div>
-        <div class="col-md-6 p-0">
-          <img src="@/assets/AboutUs.jpg" alt="" class="homepic">
-        </div>
+      <div class="slogan-bg row">
+          <div class="col-md-6 p-0 order-1">
+            <img src="@/assets/bouquet.jpg" alt="" style="width:85%; display:block; margin:auto;"
+            class="rounded-3">
+          </div>
+          <div class="col-md-6 text-light order-2">
+            <h4 class="fst-italic text-danger slogan-title">
+              不再只是曇花一現...
+            </h4>
+            <h3 class="fst-italic text-center w-100 slogan-content">
+              用永生花表達愛意，<br>
+              讓永恆的感動一直在心中盛開。
+            </h3>
+          </div>
+      </div>
+      <div class="slogan-bg row mt-5">
+          <div class="col-md-6 p-0 order-md-2">
+            <img src="@/assets/AboutUs.jpg" alt="" style="width:85%; display:block; margin:auto;"
+            class="rounded-3">
+          </div>
+          <div class="col-md-6 align-middle text-light order-md-1">
+            <h4 class="fst-italic text-danger slogan-title">
+              不再只是一時的歡愉體驗...
+            </h4>
+            <h3 class="fst-italic text-center w-100 slogan-content">
+              用永生花，<br>
+              為每個特別的時刻留下永恆的回憶
+            </h3>
+          </div>
       </div>
       <!-- 新品上市 -->
-      <div class="mt-3 mb-3 row newItem-bg py-2">
-        <div class="col-md-3 newItem mt-md-0 mt-3">
+      <div class="mt-4 row newItem-bg py-2">
+        <div class="col-lg-3 newItem mt-md-0 py-md-2">
           <h1 class="text-center"><i class="bi bi-flower3 text-danger"></i>新品上市</h1>
         </div>
-        <div class="row col-md-9 px-0">
-          <div class="col-sm-12 col-md-4 px-0"
+        <div class="row col-lg-9 px-0 mx-auto">
+          <div class="col-sm-12 col-md-4"
             v-for="(item, key) in newProductList" :key="key">
             <div class="card mt-1 mb-2 mx-auto" style="max-width: 280px;">
               <a href="#" class="text-decoration-none"
               @click.prevent="goForDetail(item.id)">
+               <!-- 商品圖片 -->
                 <div class="overflow-hidden text-light position-relative border-bottom"
                   style="height: 345px;">
                   <img :src="item.imageUrl" alt=""
@@ -100,13 +122,19 @@
   background-size: cover;
   background-position:center;
 }
-.homepic{
-  border-radius: 46% 54% 30% 70% / 53% 43% 57% 47%;
-  width: 88%;
+.slogan-title{
+  position: absolute;
+  padding-top: 1%;
 }
-.slogan{
-  width: 290px;
-  padding-top: 20%;
+.slogan-content {
+  position: relative;
+  top: 50%;
+  transform: translate(0, -50%);
+}
+.slogan-bg {
+  background:
+  linear-gradient(transparent 20%,rgba(231, 152, 152, .7) 20%,
+  rgba(231, 152, 152, .7) 80%, transparent 80%);
 }
 @media only screen and (max-width: 768px){
   .homepic{
@@ -115,13 +143,13 @@
     padding: 0;
     position: relative;
   }
-  .slogan{
-    /* color: white; */
-    text-shadow: rgba(231, 152, 152, 1) 0.1em 0.1em 0.2em;
-    color: white;
-    position: absolute;
+  .slogan-title{
     z-index: 5;
-    padding-top: 10px;
+  }
+  .slogan-content{
+    color: rgba(231, 152, 152, 1);
+    margin-top: 20px;
+    z-index: 5;
   }
 }
 @keyframes move{
@@ -130,8 +158,7 @@
   100% {transform:translateY(0px);}
 }
 .newItem-bg {
-  background:linear-gradient(135deg,rgba(231, 152, 152, 0.6) 30%,#e9ecef );
-  /* linear-gradient(135deg,#e9ecef 40%, rgba(231, 152, 152, 1)) */
+  background:linear-gradient(135deg,rgba(231, 152, 152, 0.7) 30%,#e9ecef );
 }
 .newItem{
   color: rgb(247, 248, 249);
@@ -141,6 +168,7 @@
   align-items: center;
 }
 </style>
+
 <script>
 import Onsalemodal from '@/components/Front/OnsaleModal.vue';
 
@@ -168,6 +196,9 @@ export default {
     openModel() {
       this.copyCode();
       this.$refs.sale.showModal();
+    },
+    goMart() {
+      this.$router.push('/products');
     },
     copyCode() {
       navigator.clipboard.writeText('Infinity8');
