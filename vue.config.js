69aca5c6923102c0d0d3149bbaf3838488c5cec9
“chinyuting” <s86052915@gmail.com>
2023-04-27 18:02:15 +0800

    fix vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
-  publicPath: '/infinity-flowers/dist/'
-});
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/infinity-flowers/dist/'
+    : '/'
+}
