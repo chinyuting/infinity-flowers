f22218e2dcb31c42483f87e66288dff1741652fb
kkmao <bra224max852zxc@gmail.com>
2023-05-03 18:45:08 +0800

    delay time edit

--- src/components/ToastMessage.vue
+++ src/components/ToastMessage.vue
@@ -24,7 +24,7 @@ export default {
   mounted() {
     const toastEl = this.$refs.toast;
     const toast = new Toast(toastEl, {
-      delay: 80000,
+      delay: 6000,
     });
     toast.show();
   },
