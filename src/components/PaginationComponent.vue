d9b3a8a1eee3350fff1d10e937126d538585751d
kkmao <bra224max852zxc@gmail.com>
2023-05-03 19:35:15 +0800

    css edit

--- src/components/PaginationComponent.vue
+++ src/components/PaginationComponent.vue
@@ -2,19 +2,19 @@
   <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-center">
       <li class="page-item">
-        <a class="page-link" href="#" tabindex="-1" aria-disabled="true"
+        <a class="page-link text-deep" href="#" tabindex="-1" aria-disabled="true"
         :class="{ disabled: !temppage.has_pre }"
         @click.prevent="updatePage(temppage.current_page-1)">上一頁</a>
       </li>
       <li class="page-item"
       v-for="(item, key) in temppage.total_pages"
       :key="key">
-        <a class="page-link" href="#"
+        <a class="page-link text-deep" href="#"
         :class=" { 'active': item===temppage.current_page } "
         @click.prevent="updatePage(item)">{{ item }}</a>
       </li>
       <li class="page-item">
-        <a class="page-link" href="#"
+        <a class="page-link text-deep" href="#"
         :class="{ disabled: !temppage.has_next }"
         @click.prevent="updatePage(temppage.current_page+1)">下一頁</a>
       </li>
@@ -42,3 +42,17 @@ export default {
   },
 };
 </script>
+
+<style>
+.text-pastel{
+  color:rgba(231, 152, 152, 1);
+}
+.text-deep {
+  color:#2A1B18;
+}
+.page-link.active{
+  background-color:rgba(231, 152, 152, .7);
+  color: #2A1B18;
+  border-color:rgba(231, 152, 152, 1);
+}
+</style>
