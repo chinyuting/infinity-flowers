22c76a674ac40b0ccd366fc9f6d93b758f9335a7
kkmao <bra224max852zxc@gmail.com>
2023-05-15 22:11:45 +0800

    nav bar toggle fix

--- src/components/Front/FontNavbar.vue
+++ src/components/Front/FontNavbar.vue
@@ -7,10 +7,10 @@
     </a>
      <!-- toggle -->
     <button class="navbar-toggler" type="button"
-    data-bs-toggle="collapse" data-bs-target=".FontnavbarNav"
+    data-bs-toggle="collapse"
+    data-bs-target=".FontnavbarNav"
     aria-controls="FontnavbarNav" aria-expanded="false"
-    aria-label="Toggle navigation" ref="navbarToggler"
-    @blur="closeNav" @click.prevent="openNav">
+    aria-label="Toggle navigation" ref="navbarToggler">
       <span class="navbar-toggler-icon"></span>
     </button>
     <!-- nav list -->
@@ -18,18 +18,21 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center text-center">
         <li class="nav-item">
           <router-link to="/" class="nav-link homenav-item"
+          @click.prevent="closeNav"
           :class="pageNow === 'home' ? 'homenav-active' : ''">
             首頁
           </router-link>
         </li>
         <li class="nav-item">
           <router-link to="/products" class="nav-link homenav-item"
+          @click.prevent="closeNav"
           :class="pageNow === 'products' ? 'homenav-active' : ''">
             產品
           </router-link>
         </li>
         <li class="nav-item">
           <router-link to="/orderSearch" class="nav-link homenav-item"
+          @click.prevent="closeNav"
           @click="pageChange('orderSearch')"
           :class="pageNow === 'orderSearch' ? 'homenav-active' : ''">
             訂單查詢
@@ -37,13 +40,15 @@
         </li>
         <li class="nav-item">
           <router-link to="/QA" class="nav-link homenav-item"
+          @click.prevent="closeNav"
           @click="pageChange('QA')"
           :class="pageNow === 'QA' ? 'homenav-active' : ''">
             常見問題
           </router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/dashboard/productmanage" class="nav-link homenav-item">
+          <router-link to="/dashboard/productmanage" class="nav-link homenav-item"
+          @click.prevent="closeNav">
             管理員登入
           </router-link>
         </li>
@@ -89,7 +94,8 @@ export default {
       cartLength: 0,
       favoriteLength: 0,
       pageNow: '',
-      navOpen: true,
+      navOpen: false,
+      navOpenStatus: false,
     };
   },
   components: {
@@ -107,13 +113,7 @@ export default {
       this.pageNow = page;
     },
     closeNav() {
-      if (this.navOpen === true) {
-        this.$refs.navbarToggler.click();
-        this.navOpens = false;
-      }
-    },
-    openNav() {
-      this.navOpen = true;
+      this.$refs.navbarToggler.click();
     },
   },
   mixins: [cartMixin, favoritesMixin],
@@ -143,11 +143,12 @@ export default {
   border-radius:16px;
   display:inline-block;
   text-align:center;
+  line-height: 16px;
   font-size:0.1em;
   color:#fff;
   background:red;
   position: relative;
-  top: -8px;
+  top: -12px;
   right: 8px;
 }
 .homenav-item{
@@ -166,9 +167,8 @@ export default {
 }
 .homenav-item:hover::after{
   width: 100%;
-  /* border-bottom: #4F332D 1px solid;
-  animation:show 2s; */
 }
+
 .homenav-active{
   color: rgba(231, 152, 152, 1);
   font-weight: 700;
@@ -182,9 +182,6 @@ export default {
 .member-function {
   flex-grow:0;
 }
-/* .member-function .nav-item{
-  margin-right:16px;
-} */
 .notice-visible{
   visibility:visible;
 }
@@ -192,6 +189,12 @@ export default {
   visibility:hidden;
 }
 @media only screen and (max-width: 991px) {
+  .homenav-item::after{
+    left: 40%;
+  }
+  .homenav-item:hover::after{
+    width: 20%;
+  }
   .member-function {
     position: absolute;
     right: 85px;
